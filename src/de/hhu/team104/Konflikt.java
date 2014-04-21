5fac1b2920e14bdfcc67bbe071b2b3fa45b41c60
Marc Schnitzler <Marc.Schnitzler@uni-duesseldorf.de>
2014-04-21 22:41:27 +0200

    Konflikt

--- src/de/hhu/team104/Konflikt.java
+++ src/de/hhu/team104/Konflikt.java
@@ -2,4 +2,11 @@
  * Created by Darc93 on 21.04.2014.
  */
 public class Konflikt {
+        public static void main(String [] args){
+            int i = 5;
+            for(int x = 0; x < 50; x++ ) {
+                i = i - 5;
+            }
+        }
+
 }
